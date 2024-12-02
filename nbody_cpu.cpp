41fd5fd28c5496a6ae2fc40deb2ecf44ffb56906
Burned357Waffles <bwatanabe@sfsu.edu>
2024-12-02 14:11:13 -0800

    improved cache util, moved history out of struct

--- nbody_cpu.cpp
+++ nbody_cpu.cpp
@@ -19,11 +19,11 @@
 
 struct Body
 {
-   float mass;
-   float velocity[3];
-   float position[3];
-   std::vector<float> velocity_history;
-   std::vector<float> position_history;
+   double mass;
+   double velocity[3];
+   double position[3];
+   std::vector<double> velocity_history;
+   std::vector<double> position_history;
 
    bool operator==(const Body& other) const
    {
@@ -35,33 +35,36 @@ struct Body
 
 char output_fname[] = "../data/positions.csv";
 
-#define G 6.67430e-11
-#define SOLAR_MASS 1.989e30
-#define MERCURY_MASS 3.285e23
-#define VENUS_MASS 4.867e24
-#define EARTH_MASS 5.972e24
-#define MARS_MASS 6.390e23
-#define JUPITER_MASS 1.898e27
-#define SATURN_MASS 5.683e26
-#define URANUS_MASS 8.681e25
-#define NEPTUNE_MASS 1.024e26
-#define ASTEROID_MASS 1.0e12
-#define AU 1.496e11
+const double G = 6.67430e-11;
+const double SOLAR_MASS = 1.989e30;
+const double MERCURY_MASS = 3.285e23;
+const double VENUS_MASS = 4.867e24;
+const double EARTH_MASS = 5.972e24;
+const double MARS_MASS = 6.39e23;
+const double JUPITER_MASS = 1.898e27;
+const double SATURN_MASS = 5.683e26;
+const double AU = 1.496e11;
+const double URANUS_MASS = 8.681e25;
+const double NEPTUNE_MASS = 1.024e26;
+const double ASTEROID_MASS = 1.0e12;
+
+std::vector<std::vector<double>> velocity_history;
+std::vector<std::vector<double>> position_history;
 
 
-std::vector<float> 
+std::vector<double> 
 compute_forces(std::vector<Body>& bodies, Body i, int N, int dimensions)
 {
-   std::vector<float> total_force(dimensions, 0.0);
+   std::vector<double> total_force(dimensions, 0.0);
 
    for(int j = 0; j < N; j++)
    {
       if(i == bodies[j])
          continue;
 
-      std::vector<float> dx(dimensions, 0.0);
-      float r = 0.0;
-      float r_norm = 0.0;
+      std::vector<double> dx(dimensions, 0.0);
+      double r = 0.0;
+      double r_norm = 0.0;
 
       for (int idx = 0; idx < dimensions; idx++)
       {
@@ -75,7 +78,7 @@ compute_forces(std::vector<Body>& bodies, Body i, int N, int dimensions)
 
       for (int idx = 0; idx < dimensions; idx++)
       {
-         float f = (G * i.mass * bodies[j].mass * dx[idx]) / (r_norm * r_norm * r_norm);
+         double f = (G * i.mass * bodies[j].mass * dx[idx]) / (r_norm * r_norm * r_norm);
          total_force[idx] += f;
       }
    }
@@ -84,7 +87,7 @@ compute_forces(std::vector<Body>& bodies, Body i, int N, int dimensions)
 }
 
 void 
-update_bodies(std::vector<Body>& bodies, std::vector<float>& forces, float dt, int N, int dimensions)
+update_bodies(std::vector<Body>& bodies, std::vector<double>& forces, double dt, int N, int dimensions)
 {
    for (int i = 0; i < N; i++)
    {
@@ -93,9 +96,13 @@ update_bodies(std::vector<Body>& bodies, std::vector<float>& forces, float dt, i
          bodies[i].velocity[idx] += forces[i * dimensions + idx] / bodies[i].mass * dt;
          bodies[i].position[idx] += bodies[i].velocity[idx] * dt;
 
-         bodies[i].velocity_history.push_back(bodies[i].velocity[idx]);
-         bodies[i].position_history.push_back(bodies[i].position[idx]);
+         //bodies[i].velocity_history.push_back(bodies[i].velocity[idx]);
+         //bodies[i].position_history.push_back(bodies[i].position[idx]);
       }
+
+      velocity_history[i].insert(velocity_history[i].end(), std::begin(bodies[i].velocity), std::end(bodies[i].velocity));
+      position_history[i].insert(position_history[i].end(), std::begin(bodies[i].position), std::end(bodies[i].position));
+    
    }
 }
 
@@ -104,12 +111,12 @@ do_nBody_calculation(std::vector<Body>& bodies, int N, int dimensions, int times
 {
    for(int t = 0; t < final_time; t+=timestep)
    {
-      std::vector<float> forces(N * dimensions, 0.0);
+      std::vector<double> forces(N * dimensions, 0.0);
 
       #pragma omp parallel for
       for(int i = 0; i < N; i++)
       {
-         std::vector<float> force = compute_forces(bodies, bodies[i], N, dimensions);
+         std::vector<double> force = compute_forces(bodies, bodies[i], N, dimensions);
          for (int idx = 0; idx < dimensions; idx++)
          {
             forces[i * dimensions + idx] = force[idx];
@@ -140,10 +147,10 @@ init_random_bodies(int dimensions, int N)
    std::vector<Body> bodies(N);
    std::random_device rd;
    std::mt19937 gen(rd());
-   //std::uniform_real_distribution<float> mass_dist(ASTEROID_MASS, ASTEROID_MASS);
-   std::uniform_real_distribution<float> mass_dist(ASTEROID_MASS, JUPITER_MASS);
-   std::uniform_real_distribution<float> velocity_dist(-50.0e3, 50.0e3); // Velocity in m/s
-   std::uniform_real_distribution<float> position_dist(-AU, AU);
+   //std::uniform_real_distribution<double> mass_dist(ASTEROID_MASS, ASTEROID_MASS);
+   std::uniform_real_distribution<double> mass_dist(ASTEROID_MASS, JUPITER_MASS);
+   std::uniform_real_distribution<double> velocity_dist(-50.0e3, 50.0e3); // Velocity in m/s
+   std::uniform_real_distribution<double> position_dist(-AU, AU);
 
    for(int i = 0; i < N; i++)
    {
@@ -159,6 +166,24 @@ init_random_bodies(int dimensions, int N)
       }
       bodies[i] = body;
    }
+
+   /*
+   bodies[0].mass = SOLAR_MASS;  
+   bodies[0].velocity[0] = 0;
+   bodies[0].velocity[1] = 0;
+   bodies[0].velocity[2] = 0;
+   bodies[0].position[0] = 0;
+   bodies[0].position[1] = 0;
+   bodies[0].position[2] = 0;
+
+   bodies[1].mass = ASTEROID_MASS;
+   bodies[1].velocity[0] = 22365.5;
+   bodies[1].velocity[1] = 24955.3;
+   bodies[1].velocity[2] = 28634.1;
+   bodies[1].position[0] = -1.77419e+09;
+   bodies[1].position[1] = 1.52822e+10;
+   bodies[1].position[2] = -2.62286e+10;
+   */
 
    // print all the bodies
    /*
@@ -177,77 +202,38 @@ std::vector<Body>
 init_solar_system(int dimensions)
 {
    std::vector<Body> bodies(9);
-   bodies[0].mass = SOLAR_MASS;
-   bodies[0].velocity[0] = 0;
-   bodies[0].velocity[1] = 0;
-   bodies[0].velocity[2] = 0;
-   bodies[0].position[0] = 0;
-   bodies[0].position[1] = 0;
-   bodies[0].position[2] = 0;
- 
-   bodies[1].mass = MERCURY_MASS;
-   bodies[1].velocity[0] = 0;
-   bodies[1].velocity[1] = 47.87e3;
-   bodies[1].velocity[2] = 0;
-   bodies[1].position[0] = 0.39 * AU;
-   bodies[1].position[1] = 0;
-   bodies[1].position[2] = 0;
-   
-   bodies[2].mass = VENUS_MASS;
-   bodies[2].velocity[0] = 0;
-   bodies[2].velocity[1] = 35.02e3;
-   bodies[2].velocity[2] = 0;
-   bodies[2].position[0] = 0.72 * AU;
-   bodies[2].position[1] = 0;
-   bodies[2].position[2] = 0;
 
-   bodies[3].mass = EARTH_MASS;
-   bodies[3].velocity[0] = 0;
-   bodies[3].velocity[1] = 29.78e3;
-   bodies[3].velocity[2] = 0;
-   bodies[3].position[0] = AU;
-   bodies[3].position[1] = 0;
-   bodies[3].position[2] = 0;
+    double masses[] = {SOLAR_MASS, MERCURY_MASS, VENUS_MASS, EARTH_MASS, MARS_MASS, JUPITER_MASS, SATURN_MASS, URANUS_MASS, NEPTUNE_MASS};
+    double velocities[][3] = {
+        {0, 0, 0},
+        {0, 47.87e3, 0},
+        {0, 35.02e3, 0},
+        {0, 29.78e3, 0},
+        {0, 24.07e3, 0},
+        {0, 13.07e3, 0},
+        {0, 9.69e3, 0},
+         {0, 6.81e3, 0},
+         {0, 5.43e3, 0}
+    };
+    double positions[][3] = {
+        {0, 0, 0},
+        {0.39 * AU, 0, 0},
+        {0.72 * AU, 0, 0},
+        {AU, 0, 0},
+        {1.52 * AU, 0, 0},
+        {5.20 * AU, 0, 0},
+        {9.58 * AU, 0, 0},
+         {19.22 * AU, 0, 0},
+         {30.05 * AU, 0, 0}
+    };
 
-   bodies[4].mass = MARS_MASS;
-   bodies[4].velocity[0] = 0;
-   bodies[4].velocity[1] = 24.07e3;
-   bodies[4].velocity[2] = 0;
-   bodies[4].position[0] = 1.52 * AU;
-   bodies[4].position[1] = 0;
-   bodies[4].position[2] = 0;
-
-   bodies[5].mass = JUPITER_MASS;
-   bodies[5].velocity[0] = 0;
-   bodies[5].velocity[1] = 13.07e3;
-   bodies[5].velocity[2] = 0;
-   bodies[5].position[0] = 5.20 * AU;
-   bodies[5].position[1] = 0;
-   bodies[5].position[2] = 0;
-
-   bodies[6].mass = SATURN_MASS;
-   bodies[6].velocity[0] = 0;
-   bodies[6].velocity[1] = 9.69e3;
-   bodies[6].velocity[2] = 0;
-   bodies[6].position[0] = 9.58 * AU;
-   bodies[6].position[1] = 0;
-   bodies[6].position[2] = 0;
-
-   bodies[7].mass = URANUS_MASS;
-   bodies[7].velocity[0] = 0;
-   bodies[7].velocity[1] = 6.81e3;
-   bodies[7].velocity[2] = 0;
-   bodies[7].position[0] = 19.22 * AU;
-   bodies[7].position[1] = 0;
-   bodies[7].position[2] = 0;
-
-   bodies[8].mass = NEPTUNE_MASS;
-   bodies[8].velocity[0] = 0;
-   bodies[8].velocity[1] = 5.43e3;
-   bodies[8].velocity[2] = 0;
-   bodies[8].position[0] = 30.05 * AU;
-   bodies[8].position[1] = 0;
-   bodies[8].position[2] = 0;
+    for (int i = 0; i < 9; ++i) {
+        bodies[i].mass = masses[i];
+        for (int j = 0; j < 3; ++j) {
+            bodies[i].velocity[j] = velocities[i][j];
+            bodies[i].position[j] = positions[i][j];
+        }
+    }
 
    return bodies;
 }
@@ -268,8 +254,8 @@ write_data_to_file(const std::vector<Body>& bodies, int N)
 
    for (int i = 0; i < N; i++)
    {
-      const std::vector<float>& pos = bodies[i].position_history;
-      const std::vector<float>& vel = bodies[i].velocity_history;
+      const std::vector<double>& pos = position_history[i];
+      const std::vector<double>& vel = velocity_history[i];
       if (pos.size() % 3 != 0) {
          std::cerr << "Error: body " << i << " does not have the correct number of position coordinates" << std::endl;
          fclose(fp);
@@ -277,13 +263,12 @@ write_data_to_file(const std::vector<Body>& bodies, int N)
       }
 
       if (vel.size() % 3 != 0) {
-         std::cerr << "Error: body " << i << " does not have the correct number of veelocities" << std::endl;
+         std::cerr << "Error: body " << i << " does not have the correct number of velocities" << std::endl;
          fclose(fp);
          exit(1);
       }
 
-
-      for (int j = 0; j < pos.size(); j += 3) {
+      for (size_t j = 0; j < pos.size(); j += 3) {
          fprintf(fp, "%d,%f,%f,%f,%f,%f,%f,%f\n", i, bodies[i].mass, vel[j], vel[j+1], vel[j+2], pos[j], pos[j+1], pos[j+2]);
       }
    }
@@ -318,9 +303,9 @@ main (int ac, char *av[])
 
    int dimensions = 3;
    int timestep = 60 * 60;
-   unsigned int final_time = timestep * 24 * 365; 
-   std::vector<Body> bodies;
+   unsigned int final_time = timestep * 24 * 365 * 300; 
 
+   std::vector<Body> bodies;
    if (N == -1)
    {
       N = 9;
@@ -330,6 +315,9 @@ main (int ac, char *av[])
    {
       bodies = init_random_bodies(dimensions, N);
    }
+
+   velocity_history.resize(bodies.size() * dimensions);
+   position_history.resize(bodies.size() * dimensions);
    
    std::cout << "Number of bodies: " << N << std::endl;
 
